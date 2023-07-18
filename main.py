75e5a7a93fa6f3581dfcf46226591d4302285ab3
Codemonkk <utkarshkashyap307@gmail.com>
2023-07-18 10:28:00 +0530

    Added moving the snake code

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 from turtle import Screen, Turtle
+import time
 
 #setting up the screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
 #Create a Snake Body
 
 starting_positions = [(0,0),(-20,0),(-40,0)]
 
+segments = []
+
 for positions in starting_positions:
     new_segment = Turtle("square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(positions)
+    segments.append(new_segment)
+
+
+#Moving the Snake
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
 
 
 
